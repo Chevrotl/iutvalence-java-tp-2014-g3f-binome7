7e5502f067527dd055bee59aabcbeae1bf473998
Chevrotl <lucas.chevrot@iut-valence.fr>
2014-02-17 16:40:51 +0100

    Creation de la classe Mastermind
    Avec javadoc de la classe + javadoc de LanceurDeMasterMind

--- src/LanceurDeMastermind.java
+++ src/LanceurDeMastermind.java
@@ -1,11 +1,22 @@
-
+/**
+ * Represente le lanceur du jeu
+ * @author chevrotl
+ *
+ */
 public class LanceurDeMastermind
 {
 
+	/**
+	 * Methode main, creant une nouvelle partie 
+	 * @param args Parametre de main
+	 */
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
-
+		System.out.println("Creation d'une partie de Mastermind");
+		Mastermind PartieDeMastermind = new Mastermind;
+		System.out.println("Demarage de la partie");		
+		PartieDeMastermind.jouer();
+		System.out.println("Fin de la partie");
 	}
 
 }
---
+++ src/Mastermind.java
@@ -0,0 +1,15 @@
+/**
+ * Represente une partie de Mastermind
+ * @author chevrotl
+ *
+ */
+public class Mastermind
+{
+	/**
+	 * Lance l'integralitee d'une partie
+	 */
+	public void jouer()
+	{
+		System.out.println("Lancement d'une partie");
+	}
+}
