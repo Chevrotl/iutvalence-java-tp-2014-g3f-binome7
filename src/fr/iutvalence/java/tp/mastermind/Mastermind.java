d19b3fac26b447e3feec3a71fac1cd13b50f13b7
Chevrotl <lucas.chevrot@iut-valence.fr>
2014-02-17 17:30:08 +0100

    Fin premiere seance TP + Constructeurs
    Creation du constructeur par defaut, + premieres constantes javadoc a
    finir

--- src/fr/iutvalence/java/tp/mastermind/Mastermind.java
+++ src/fr/iutvalence/java/tp/mastermind/Mastermind.java
@@ -1,4 +1,4 @@
-package fr.iutvalence.java.tp.mastermind
+package fr.iutvalence.java.tp.mastermind;
 /**
  * Represente une partie de Mastermind
  * @author chevrotl
@@ -6,11 +6,24 @@ package fr.iutvalence.java.tp.mastermind
  */
 public class Mastermind
 {
+	//Constantes
+	public final static int DEFAUT_NB_PIONS_SECRET = 5;
+	
+	// attributs
+	private int nbPionsSecret ;
+	
+	//constructeurs
+	public Mastermind()
+	{
+		this.nbPionsSecret = DEFAUT_NB_PIONS_SECRET ;
+	}
+	
 	/**
 	 * Lance l'integralitee d'une partie
 	 */
 	public void jouer()
 	{
 		System.out.println("Lancement d'une partie");
+		
 	}
 }
