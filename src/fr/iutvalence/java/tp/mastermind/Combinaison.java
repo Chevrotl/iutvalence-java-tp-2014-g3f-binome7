69eac38c256bc4d2116d65473ef4fe797cd2d4f3
Chevrotl <lucas.chevrot@iut-valence.fr>
2014-02-24 17:28:40 +0100

    Modification classe Combinaison

--- src/fr/iutvalence/java/tp/mastermind/Combinaison.java
+++ src/fr/iutvalence/java/tp/mastermind/Combinaison.java
@@ -1,6 +1,30 @@
 package fr.iutvalence.java.tp.mastermind;
-
+/**
+ * Combinaison de pions, suivant plusieurs parametres
+ * @author chevrotl
+ *
+ */
 public class Combinaison
 {
 
+	/**
+	 * couleur du pion par défaut
+	 */
+	private int couleurDuPion ;
+	/**
+	 * Contructeur par defaut créant un tableau de pions aléatoire d'une longeur par défaut
+	 */
+	
+	
+	/**
+	 * Creation d'un tableau de pion avec une taille donnée 
+	 * @param tailleCombinaisonAleatoire
+	 */
+	public Combinaison(int tailleCombinaisonAleatoire)
+	{
+		Pion[] combinaisonADecouvrir = new Pion[tailleCombinaisonAleatoire];
+		
+		
+	}
+	
 }
