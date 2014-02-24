52c988490aafdbc3ed384129433d15c4643b598b
Chevrotl <lucas.chevrot@iut-valence.fr>
2014-02-24 16:31:46 +0100

    Creation classe Pion + correction Couleur
    Mise en majuscule des noms de couleurs

--- src/fr/iutvalence/java/tp/mastermind/Couleur.java
+++ src/fr/iutvalence/java/tp/mastermind/Couleur.java
@@ -10,41 +10,44 @@ public class Couleur
 	/**
 	 * Definition d'une palette limitée de couleur, gérée par des entiers
 	 */
-	public final static int blanc = 1 ;
+	 
+
+	
+	public final static int BLANC = 1 ;
 	/**
 	 * Definition couleur rouge
 	 */
-	public final static int rouge = 2 ;
+	public final static int ROUGE = 2 ;
 	
 	/**
 	 * Definition couleur jaune
 	 */
-	public final static int jaune = 3 ;
+	public final static int JAUNE = 3 ;
 	
 	/**
 	 * Definition couleur vert
 	 */
-	public final static int vert = 4 ;
+	public final static int VERT = 4 ;
 	
 	/**
 	 * Definition couleur bleu
 	 */
-	public final static int bleu = 5 ;
+	public final static int BLEU = 5 ;
 	
 	/**
 	 * Definition couleur violet
 	 */
-	public final static int violet = 6 ;
+	public final static int VIOLET = 6 ;
 	
 	/**
 	 * Definition couleur rose
 	 */
-	public final static int rose = 7 ;
+	public final static int ROSE = 7 ;
 	
 	/**
 	 * Definition couleur orange
 	 */
-	public final static int orange = 8 ;
+	public final static int ORANGE = 8 ;
 	
 	
 }
