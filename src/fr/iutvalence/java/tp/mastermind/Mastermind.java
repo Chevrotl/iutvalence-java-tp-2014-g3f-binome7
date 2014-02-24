e8b90c2c4a9c59e2349bee8a70a5ef5d27f5de27
sebastienjean <baz.jean@gmail.com>
2014-02-24 14:17:12 +0100

    survol du code, ajout de remarques

--- src/fr/iutvalence/java/tp/mastermind/Mastermind.java
+++ src/fr/iutvalence/java/tp/mastermind/Mastermind.java
@@ -7,19 +7,25 @@ package fr.iutvalence.java.tp.mastermind;
 public class Mastermind
 {
 	//Constantes
+	// TODO renommer (plus lisible)
+	// TODO écrire le commentaire
 	public final static int DEFAUT_NB_PIONS_SECRET = 5;
 	
 	// attributs
+	// TODO renommer (plus lisible)
+	// TODO écrire le commentaire
 	private int nbPionsSecret ;
 	
 	//constructeurs
+	// TODO écrire le commentaire
 	public Mastermind()
 	{
 		this.nbPionsSecret = DEFAUT_NB_PIONS_SECRET ;
 	}
 	
+	// TODO écrire un commentaire plus précis
 	/**
-	 * Lance l'integralitee d'une partie
+	 * Lance l'integralite d'une partie
 	 */
 	public void jouer()
 	{
