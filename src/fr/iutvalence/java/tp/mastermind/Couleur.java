39b3dfb9b0d00b56e521cc0b6a537a2dfe9fb0b5
Chevrotl <lucas.chevrot@iut-valence.fr>
2014-02-24 16:14:59 +0100

    Creation de la classe couleur
    Creation de la classe couleur pour gerer celle-ci avec des entiers.

---
+++ src/fr/iutvalence/java/tp/mastermind/Couleur.java
@@ -0,0 +1,30 @@
+package fr.iutvalence.java.tp.mastermind;
+/**
+ * Definition des 8 couleurs utilisable pour trouver la combinaison
+ * @author chevrotl
+ *
+ */
+public class Couleur
+{
+
+	/**
+	 * Definition d'une palette limitée de couleur, gérée par des entiers
+	 */
+	public final static int blanc = 1 ;
+	
+	public final static int rouge = 2 ;
+	
+	public final static int jaune = 3 ;
+	
+	public final static int vert = 4 ;
+	
+	public final static int bleu = 5 ;
+	
+	public final static int violet = 6 ;
+	
+	public final static int rose = 7 ;
+	
+	public final static int orange = 8 ;
+	
+	
+}
