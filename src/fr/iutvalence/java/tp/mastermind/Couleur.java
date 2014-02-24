4b1c937812d17c3eaf9a67f23d4ed5c9e434aeb2
Chevrotl <lucas.chevrot@iut-valence.fr>
2014-02-24 16:17:59 +0100

    Correction javadoc Couleur

--- src/fr/iutvalence/java/tp/mastermind/Couleur.java
+++ src/fr/iutvalence/java/tp/mastermind/Couleur.java
@@ -11,19 +11,39 @@ public class Couleur
 	 * Definition d'une palette limitée de couleur, gérée par des entiers
 	 */
 	public final static int blanc = 1 ;
-	
+	/**
+	 * Definition couleur rouge
+	 */
 	public final static int rouge = 2 ;
 	
+	/**
+	 * Definition couleur jaune
+	 */
 	public final static int jaune = 3 ;
 	
+	/**
+	 * Definition couleur vert
+	 */
 	public final static int vert = 4 ;
 	
+	/**
+	 * Definition couleur bleu
+	 */
 	public final static int bleu = 5 ;
 	
+	/**
+	 * Definition couleur violet
+	 */
 	public final static int violet = 6 ;
 	
+	/**
+	 * Definition couleur rose
+	 */
 	public final static int rose = 7 ;
 	
+	/**
+	 * Definition couleur orange
+	 */
 	public final static int orange = 8 ;
 	
 	
