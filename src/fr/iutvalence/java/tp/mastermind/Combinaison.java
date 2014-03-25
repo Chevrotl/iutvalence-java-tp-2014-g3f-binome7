bc7835b7e5d9bed00bd65379e703edf37810fb8a
Chevrotl <lucas.chevrot@iut-valence.fr>
2014-03-25 17:26:26 +0100

    FIN TP5 pour de vrai
    lel

--- src/fr/iutvalence/java/tp/mastermind/Combinaison.java
+++ src/fr/iutvalence/java/tp/mastermind/Combinaison.java
@@ -1,83 +1,84 @@
 package fr.iutvalence.java.tp.mastermind;
+
 /**
  * Combinaison de pions, suivant plusieurs parametres
+ * 
  * @author chevrotl
- *
+ * 
  */
 public class Combinaison
 {
 
 	/**
-	 * declaration du tableau de pions pour ne pas que ce soit une variable locale
+	 * declaration du tableau de pions pour ne pas que ce soit une variable
+	 * locale
 	 */
-	private Pion[] pions ;
-	
-	
+	private Pion[] pions;
+
 	/**
-	 * Renvoi une representation numerique des couleur sous la forme d'une chaine
+	 * Renvoi une representation numerique des couleur sous la forme d'une
+	 * chaine
 	 */
 	public String toString()
 	{
-		String result ="";
-		
-		for (int i = 0; i < this.pions.length ; i++ )
+		String result = "";
+
+		for (int i = 0; i < this.pions.length; i++)
 		{
-			result += this.pions[i].obtenirCouleur()+" ";
+			result += this.pions[i].obtenirCouleur() + " ";
 		}
-		
+
 		return result;
-		
+
 	}
-	
-		
+
 	/**
 	 * Creation d'une combinaison aléatoire, de taille donnée
-	 * @param taille taille de la combinaison a découvrir
+	 * 
+	 * @param taille
+	 *            taille de la combinaison a découvrir
 	 */
 	public Combinaison(int taille)
 	{
 		this.pions = new Pion[taille];
-		
-		for(int numeroDuPion = 0; numeroDuPion < taille ; numeroDuPion++)
-			this.pions[numeroDuPion] = new Pion ();
+
+		for (int numeroDuPion = 0; numeroDuPion < taille; numeroDuPion++)
+			this.pions[numeroDuPion] = new Pion();
 
 	}
-	
-	
 
-	
 	public ResultatComparaison comparerAvec(Combinaison autreCombinaison)
 	{
 
 		int nombreDePionsBienPlaces = 0;
-		int nombreDePionsMalPlaces = 0; 
-		boolean [] pionCompare = new boolean[this.pions.length];  
+		int nombreDePionsMalPlaces = 0;
+		boolean[] pionANegliger = new boolean[this.pions.length];
 
-
-
-		for (int numeroDuPion = 0 ; numeroDuPion < this.pions.length ; numeroDuPion++ )
+		for (int numeroDuPion = 0; numeroDuPion < this.pions.length; numeroDuPion++)
 		{
 
 			if ((this.pions[numeroDuPion].equals(autreCombinaison.pions[numeroDuPion])))
 			{
-				nombreDePionsBienPlaces++; 
-				pionCompare[numeroDuPion] = true ;				
+				nombreDePionsBienPlaces++;
+				pionANegliger[numeroDuPion] = true;
+				continue;
 			}
-			else
+		}
+		
+		for (int numeroDuPion = 0; numeroDuPion < this.pions.length; numeroDuPion++)
+		{
+			if (pionANegliger[numeroDuPion]) continue;
+			
+			for (int numeroDuPionAutreCombinaison = 0; numeroDuPionAutreCombinaison < this.pions.length; numeroDuPionAutreCombinaison++)
 			{
+				if (pionANegliger[numeroDuPionAutreCombinaison])
+					continue;
 
-				for (int numeroDuPionAutreCombinaison = 0 ; numeroDuPionAutreCombinaison < this.pions.length ; numeroDuPionAutreCombinaison++)
+				if (this.pions[numeroDuPion].equals(autreCombinaison.pions[numeroDuPionAutreCombinaison]))
 				{
-					if (pionCompare[numeroDuPionAutreCombinaison]) 
-						continue;
-
-					if (this.pions[numeroDuPion].equals(autreCombinaison.pions[numeroDuPionAutreCombinaison]))
-					{
-						nombreDePionsMalPlaces++;
-						pionCompare[numeroDuPionAutreCombinaison]=true;
-						break;
-					}
-
+					nombreDePionsMalPlaces++;
+					pionANegliger[numeroDuPionAutreCombinaison] = true;
+					break;
 				}
 
 			}
@@ -87,8 +88,6 @@ public class Combinaison
 		System.out.println("retour");
 		return new ResultatComparaison(nombreDePionsBienPlaces, nombreDePionsMalPlaces);
 
-
 	}
 
-	
 }
